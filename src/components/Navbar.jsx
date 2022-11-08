9e1deee702042c8ba60f6f00929190d54cdf1577
lil-dev16 <olusegunkd2017@gmail.com>
2022-11-08 22:31:07 +0100

    feat: nav

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='nav__bar'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Classes</li>
+            <li>Students</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}

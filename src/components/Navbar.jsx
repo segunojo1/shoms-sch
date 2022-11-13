b88f614d7934e4c68256412e22b6eb85de072bdd
lil-dev16 <olusegunkd2017@gmail.com>
2022-11-13 23:16:14 +0100

    add tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,11 @@ import { NavLink } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <div className='nav__bar'>
-        <ul>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/test">About</NavLink>
-            <NavLink to="/">Contact</NavLink>
-            <NavLink to="/">Form</NavLink>
+        <ul className='flex w-[80%] justify-between items-center h-[6rem] mx-auto'>
+            <NavLink to="/" className="nav__item">OUR SCHOOL</NavLink>
+            <NavLink to="/test" className="nav__item">CLASSES</NavLink>
+            <NavLink to="/" className="nav__item">REGISTRATION</NavLink>
+            <NavLink to="/" className="btn__enroll nav__item">ENROLL NOW</NavLink>
         </ul>
     </div>
   )

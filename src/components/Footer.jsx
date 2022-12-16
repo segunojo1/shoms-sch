aebbe10558209bf748c7310538172bc83ca81c71
lil-dev16 <olusegunkd2017@gmail.com>
2022-12-16 11:48:51 +0100

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,35 +6,33 @@ const Footer = () => {
     <div className='bg-black '>
         <div className='mx-auto w-[80%] container text-white flex flex-col md:flex-row justify-between py-[3rem]'>
             <div>
-                <img src="Group.svg" alt="metaNfts" className='mb-[5rem]'/>
+                
                 <div className='mb-[2rem] flex items-center justify-between w-[6rem]'>
                     <img src="facebook.png" alt="" />
                     <img src="ig.png" alt="" />
                     <img src="twitter.png" alt="" />
                 </div>
-                <p className='text-white'>2022 Metabnb</p>
+                <p className='text-white'>2022 Shoms Int'l Sch</p>
             </div>
             <FooterComp 
-                head="School"
-                one="Students"
-                two="Classes"
-                three="Staff"
-                four="Kids"
+                head="2, Odutola street"
+                one="off-Nnobi"
+                two="kilo"
+                three="Surulere"
             />
             <FooterComp 
-                head="Places"
-                one="Castle"
-                two="Farms"
-                three="Beach"
+                head="School"
+                one="Classes"
+                two="Staff"
                 four="Learn More"
             />
-            <FooterComp 
+            {/* <FooterComp 
                 head="About Us"
                 one="Road Map"
                 two="Creators"
                 three="Career"
                 four="Contact Us"
-            />
+            /> */}
         </div>
     </div>
   )
